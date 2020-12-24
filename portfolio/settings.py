a873386c4a679d447df2a66b68b823bc0906f5b0
jakob lumpkin <lumpkinjakobr@gmail.com>
2020-12-24 05:50:58 -0600

    dell

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^obdws&f#p=b%n$=wl2*+o&97i%mg+fp!9ye6es&!w4hytlqg!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['jakobsportfolio.herokuapp.com', '127.0.0.1', 'jakobsportfolio.com']
 
